77c57d0c98b2c4b2402117c78b9891a363ce6f73
Thomas Birru <tomia120@hotmail.com>
2025-08-05 17:35:37 -0400

    Fix: Added useState, todoListItem, and corrected map in TodoList

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import TodoForm from './TodoForm';
 import TodoList from './TodoList';
 
 function App() {
 
+  const [newTodo, setNewTodo] = useState("Learn React");
+
   return (
     <div>
       <h1>My Todo App</h1>
       <TodoForm />
+      <p>{newTodo}</p> {/* <-- Display newTodo */}
       <TodoList />
     </div>
   );
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,13 +1,15 @@
+import TodoListItem from './TodoListitem';
 function TodoList() {
     const todos =[
         {id: 1, title: 'Learn React'},
         {id: 2, title: 'Build a Todo App'},
+        {id: 3, title: 'Submit Assignment'},
     
     ];
     return (
       <ul>
         {todos.map((todo)=>(
-          <li key={todo.id}>{WebTransportDatagramDuplexStream.title}</li>
+          <TodoListItem key={todo.id} todo={todo} />
         ))}
         </ul>
     );
